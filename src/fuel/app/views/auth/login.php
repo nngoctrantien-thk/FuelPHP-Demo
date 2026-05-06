88f78e706d0be0681314e9bb939bb860ad7d65aa
nngoctrantien-thk <n_ngoctrantien@thk-hd.vn>
2026-05-06 15:34:02 +0700

    add action register user

--- src/fuel/app/views/auth/login.php
+++ src/fuel/app/views/auth/login.php
@@ -2,6 +2,9 @@
     <input type="text" name="username" placeholder="Username">
     <input type="password" name="password" placeholder="Password">
     <button type="submit">Đăng nhập</button>
+    <button type="button" onclick="window.location.href='<?php echo Uri::create('auth/register'); ?>'">
+        Đăng ký
+    </button>
     <?php if (isset($error)): ?>
         <p style="color: red;"><?php echo $error; ?></p>
     <?php endif; ?>
--- src/fuel/app/views/auth/register.php
+++ src/fuel/app/views/auth/register.php
@@ -1,1 +1,10 @@
 <p>Register</p>
+<form method="post">
+    <input type="text" name="username" placeholder="Username">
+    <input type="password" name="password" placeholder="Password">
+    <input type="email" name="email" placeholder="Email">
+    <button type="submit">Đăng ký</button>
+    <?php if (isset($error)): ?>
+        <p style="color: red;"><?php echo $error; ?></p>
+    <?php endif; ?>
+</form>
